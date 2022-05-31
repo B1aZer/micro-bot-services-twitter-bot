fd9a860bee8355a7b35f07aa3adf9f03ef49797f
concave-dash <kwerkee12@gmail.com>
2022-05-31 14:57:11 -0400

    save old logs on restart

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.nvm/nvm.sh
 nvm use v16.13.1
-node server.js | ts '[%Y-%m-%d %H:%M:%.S]' > server-twitter.log
+node server.js | ts '[%Y-%m-%d %H:%M:%.S]' >> server-twitter.log
