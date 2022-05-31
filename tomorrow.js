cadeab7bd1ee1b3e9c73bcc3e5ff38b340bda1d2
concave-dash <kwerkee12@gmail.com>
2022-05-30 22:42:53 -0400

    group up tomorrow text

--- tomorrow.js
+++ tomorrow.js
@@ -6,35 +6,43 @@ const { pickRandom, removeAndReturnRandom, randomIntFromInterval } = require('..
 init();
 
 async function init() {
-    const { data: top5 } = await axios.get(`${process.env.LOGGER_URL}/get`, {
+    const { data: top10 } = await axios.get(`${process.env.LOGGER_URL}/get`, {
         params: {
             name: process.env.LOG_NAME,
             id: 'recent',
-            limit: 5,
-            order: 'followers_count'
+            limit: 10,
+            order: 'followers_count',
+            groupBy: 5,
         }
     })
 
-    for (let i = 0; i < top5.length; i++) {
-        const el = top5[i];
+    for (let i = 0; i < top10.length; i++) {
+        const top5 = top10[i];
         try {
             const osHashes = ['#Ethereum', '#NFTs', '#NFT', '#ETH', '#Ethereum', '#NFTProject', '#Mint', ``];
-            const lines = [`Don't miss out!`, `Check it out!`, `Mark you calendars!`, `Prepare!`, `Get ready!`];
+            const tomorrowDate = new Date();
+            tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+            const month = tomorrowDate.toLocaleString('default', { month: 'long' });
+            const day = tomorrowDate.toLocaleDateString("en-GB", {
+                day: "2-digit",
+            });
+            const header = `Top mints for Tomorrow (${month} ${day} EST):`;
+            const body = top5.map(el => {
+                return `@${el.link.split('/')[el.link.split('/').length - 1]}
+Mint: ${new Date(el.date).toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true
+})}
+Followers: ${el.followers_count}
+`;
+            }).join('');
             await axios.post(`${process.env.TWITTER_URL}`, {
                 username: process.env.TWITTER_USERNAME,
-                text: `${el.name} @${el.link.split('/')[el.link.split('/').length - 1]}
-
-Will be minted on ${new Date(el.date).toString()}.
-
-${removeAndReturnRandom(lines, Math.random())}
-
-${pickRandom(osHashes, Math.random())}
-    
-${el.link}
-`
+                text: header + `\n\n` + body + `\n` + pickRandom(osHashes, Math.random()),
             });
-            // wait 2-4h
-            await new Promise(r => setTimeout(r, randomIntFromInterval(2, 4, Math.random()) * 60000 * 60));
+            // wait 10-20m
+            await new Promise(r => setTimeout(r, randomIntFromInterval(10, 20, Math.random()) * 60000));
         } catch (err) {
             console.log(err);
             continue;
