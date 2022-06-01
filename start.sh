4e68da86b3ec68a8f9282ff22b9cf1253e77291c
concave-dash <kwerkee12@gmail.com>
2022-06-01 00:10:41 -0400

    log rotation

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source ~/.nvm/nvm.sh
 nvm use v16.13.1
-node server.js | ts '[%Y-%m-%d %H:%M:%.S]' >> server-twitter.log
+cp server-twitter.log server-twitter-prev.log
+node server.js | ts '[%Y-%m-%d %H:%M:%.S]' > server-twitter.log
