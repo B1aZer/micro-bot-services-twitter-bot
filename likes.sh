9efdf1f60b9e64807e300c48e36ab20bf587ab85
concave-dash <kwerkee12@gmail.com>
2022-06-01 14:59:06 -0400

    turn off 2 bots likes in script job

--- likes.sh
+++ likes.sh
@@ -4,8 +4,8 @@ nvm use v16.13.1 > /dev/null
 cd /home/hipi/Sites/GooDee/twitter
 TWITTER_NAME="goodeefi" node likes.js | ts '[%Y-%m-%d %H:%M:%.S]' >> likes.log
 sleep 1m
-TWITTER_NAME="GooDeeBotMint" node likes.js | ts '[%Y-%m-%d %H:%M:%.S]' >> likes.log
-sleep 1m
-TWITTER_NAME="GooDeeBotNew" node likes.js | ts '[%Y-%m-%d %H:%M:%.S]' >> likes.log
-sleep 1m
+#TWITTER_NAME="GooDeeBotMint" node likes.js | ts '[%Y-%m-%d %H:%M:%.S]' >> likes.log
+#sleep 1m
+#TWITTER_NAME="GooDeeBotNew" node likes.js | ts '[%Y-%m-%d %H:%M:%.S]' >> likes.log
+#sleep 1m
 TWITTER_NAME="GooDeeBotOS" node likes.js | ts '[%Y-%m-%d %H:%M:%.S]' >> likes.log
